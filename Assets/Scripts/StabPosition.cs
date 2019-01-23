4a0983ab036e88b4ef2a19cb1779dd3e5cf2d836
lonsdale341 <lonsdale341@gmail.com>
2019-01-23 14:10:35 +0200

    freez animation

---
+++ Assets/Scripts/StabPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StabPosition : MonoBehaviour
+{
+    private Vector3 pos;
+	// Use this for initialization
+	void Start ()
+	{
+	    pos = gameObject.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    gameObject.transform.localPosition = pos;
+	}
+}
